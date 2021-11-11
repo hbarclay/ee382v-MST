0b8040b542d6009571826f683eeb3d2800b8fce5
weilincao <weilincao@utexas.edu>
2021-11-10 18:00:20 -0600

    change graph interface

--- graph.h
+++ graph.h
@@ -4,19 +4,18 @@
 #include <cstring>
 #include <limits>
 
-#define MAX_WEIGHT 10
+#define MAX_WEIGHT 100
 #define INF std::numeric_limits<int>::max()
 
 class Graph {
 
  public:
 	Graph() = delete;
-	Graph(int _V, int _E) : V(_V), E(_E) {
+	Graph(int _V) : V(_V) {
 		adjMatrix = new int[V*V];
 		for (int i = 0; i < V*V; i++) {
 			adjMatrix[i] = INF;
 		}
-		generateGraph();
 	}
 
 	Graph(const int* w, const int _V) : V(_V){
@@ -29,6 +28,7 @@ class Graph {
 	Graph& operator=(const Graph& other) {
 		if (this == &other) return *this;
 		V = other.size();
+		E = other.size();
 		int* newW = new int[V*V];
 		std::memcpy(newW, other.raw(), V*V*sizeof(int));
 		delete[] adjMatrix;
@@ -45,20 +45,21 @@ class Graph {
 	void addEdge(int src, int dst, int w) {
 		adjMatrix[src*V+dst] = w;
 		adjMatrix[dst*V+src] = w;
+		E++;
 	}
 
 	void printEdges();
 	void printVertices();
-
+	void generateConnectedGraph(int _E);
+	void generateConnectedGraphWithDensity(int density);
 	int* raw() const { return adjMatrix; }
 	int size() const { return V; }
 
  private:
-	int V;
-	int E;
+	int V=0;
+	int E=0;
 	int* adjMatrix;
 
-	void generateGraph();
 };
 
 #endif // __GRAPH_H_
--- main.cpp
+++ main.cpp
@@ -4,11 +4,37 @@
 
 #include "graph.h"
 
-#define V 20
-#define E 10
-
 int main() {
-	Graph g(V, E);
-	g.printEdges();
-	printf("prim mst hybrid: %d\n", prim_mst_hybrid(g));
+	Graph g5(5);
+	g5.addEdge(0,1,5);
+	g5.addEdge(0,2,4);
+	g5.addEdge(1,2,3);
+	g5.addEdge(1,3,7);
+	g5.addEdge(2,3,9);
+	g5.addEdge(2,4,11);
+	g5.addEdge(3,4,2); //should be 16
+	
+	Graph g8(8);
+	g8.addEdge(0,1,4);
+	g8.addEdge(0,2,6);
+	g8.addEdge(0,3,16);
+	g8.addEdge(1,5,24);
+	g8.addEdge(2,5,23);
+	g8.addEdge(2,4,5);
+	g8.addEdge(2,3,8);
+	g8.addEdge(3,4,10);
+	g8.addEdge(3,7,21);
+	g8.addEdge(4,5,18);
+	g8.addEdge(4,6,11);
+	g8.addEdge(4,7,14);
+	g8.addEdge(5,6,9);
+	g8.addEdge(6,7,7); //should be 50
+	printf("prim mst hybrid on graph g8: %d\n", prim_mst_hybrid(g8));
+
+	Graph g9(9);
+	g9.generateConnectedGraph(11);
+
+	Graph g10(10);
+	g10.generateConnectedGraphWithDensity(50);
+
 }
