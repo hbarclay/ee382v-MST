9779fb50b50850a952e129c9abdcfb9cf0e64f79
weilincao <weilincao@utexas.edu>
2021-11-11 22:04:57 -0600

    created complete parallel prim implementation(i.e. scan and hash_to_vector now perform in gpu )

--- prim_mst_gpu.cu
+++ prim_mst_gpu.cu
@@ -7,6 +7,9 @@
 #include "prim_mst_gpu.h"
 #include <sys/time.h>
 
+#include <thrust/scan.h>
+#include <thrust/device_vector.h>
+
 int* graph_gpu;
 int* d_gpu;
 int* isFixed_gpu;
@@ -17,6 +20,8 @@ int* T_gpu;
 int* parent_gpu;
 int* MWE_gpu;
 int* scan_gpu;
+
+int* scan_result_gpu;
 
 
 
@@ -159,6 +164,41 @@ void ExclusivePrefixSum(int* input, int* output, int size)
 	for (int i = 1; i < size; i++)
 		output[i] = output[i-1] + input[i-1];
 }
+
+__global__ void Q_zeroes_out(int*graph, int V, int* Q)
+{
+	int global_idx=blockDim.x * blockIdx.x + threadIdx.x;
+	if(global_idx<V)
+		Q[global_idx]=0;
+}
+__global__ void hash_to_vector(int*graph, int V, int* R, int*R_next, int*scan_result )
+{
+	int global_idx=blockDim.x * blockIdx.x + threadIdx.x;
+	if(global_idx==1)
+	{
+		R_size=scan_result[V-1]+R_next[V-1];
+	}
+	if(global_idx<V)
+	{
+		if(R_next[global_idx]==1)
+			R[scan_result[global_idx]]=global_idx;
+	}
+	cudaDeviceSynchronize();
+	R_next[global_idx]=0; //reinitialize R_next
+
+}
+
+
+void get_next_R_gpu(int* graph, int V, int num_block, int num_thread)
+{
+	thrust::device_ptr<int> R_next_thrust(R_next_gpu);
+	thrust::device_ptr<int> scan_result_thrust(scan_result_gpu);
+	thrust::exclusive_scan(R_next_thrust, R_next_thrust + V, scan_result_thrust);
+	hash_to_vector<<<num_block ,num_thread>>>(graph_gpu, V, R_gpu, R_next_gpu, scan_result_gpu);
+	cudaDeviceSynchronize();
+}
+
+
 void get_next_R(int* graph, int V)
 {
 	int temp[V];
@@ -192,6 +232,8 @@ void gpu_var_init(int* graph, int V){
 	cudaMalloc((void**)&parent_gpu,V*sizeof(int));
 	cudaMalloc((void**)&MWE_gpu,V*sizeof(int));
 
+	cudaMalloc((void**)&scan_result_gpu,V*sizeof(int));
+
 	cudaMemcpy(graph_gpu, graph, V*V*sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_gpu, d, V*sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(isFixed_gpu, isFixed, V*sizeof(int), cudaMemcpyHostToDevice);
@@ -215,10 +257,110 @@ void gpu_var_free()
 	cudaFree(T_gpu);
 	cudaFree(parent_gpu);
 	cudaFree(MWE_gpu);
+
+	cudaFree(scan_result_gpu);
 }
 
-
 int prim_mst_hybrid(Graph& g, int& time)
+{
+	struct timeval start;
+	struct timeval end;
+
+	int* graph = g.raw();
+	int V = g.size();
+	var_init(graph, V);
+	d[0]=0;
+	MinHeap H(V);
+	H.insert(0,d[0]);
+	isFixed[0]=1;
+	gpu_var_init(graph, V);
+	
+	gettimeofday(&start, NULL);
+	while(!H.empty())
+	{
+		MinHeapNode min = H.extractMin();
+		int j = min.val;
+		int j_weight=min.key;
+		R[R_size]=j;
+		R_size++;
+		
+		if(parent[j] != -1 && !isFixed[j] ){
+			isFixed[j] = true;
+			//printf("vertex %d (%d-%d,%d) is now fixed due to min cut\n",j,j,parent[j],j_weight );
+			T[j]=graph[j*V+parent[j]];
+		}
+
+		while(R_size!=0){
+			int block_dim = (V*R_size<512)?V*R_size:512 ;
+			int grid_dim = (V*R_size+block_dim-1)/block_dim; //ceiling of V*R_size/block_dim
+		
+			cudaMemcpy(d_gpu, d, V*sizeof(int), cudaMemcpyHostToDevice);
+			cudaMemcpy(isFixed_gpu, isFixed, V*sizeof(int), cudaMemcpyHostToDevice);
+			cudaMemcpy(R_gpu, R, V*sizeof(int), cudaMemcpyHostToDevice);
+			cudaMemcpy(T_gpu, T, V*sizeof(int), cudaMemcpyHostToDevice);
+
+			processEdge1GPU<<<grid_dim,block_dim>>>(
+					graph_gpu,
+					V, 
+					d_gpu,
+					isFixed_gpu,
+					R_gpu,
+					R_next_gpu,
+					Q_gpu,
+					T_gpu,
+					parent_gpu,
+					MWE_gpu
+					);
+			cudaDeviceSynchronize();
+			
+			cudaMemcpy(parent, parent_gpu, V*sizeof(int), cudaMemcpyDeviceToHost);
+			cudaMemcpy(d, d_gpu, V*sizeof(int), cudaMemcpyDeviceToHost);
+			cudaMemcpy(T, T_gpu, V*sizeof(int), cudaMemcpyDeviceToHost);
+			cudaMemcpy(Q, Q_gpu, V*sizeof(int), cudaMemcpyDeviceToHost);
+			cudaMemcpy(isFixed, isFixed_gpu, V*sizeof(int), cudaMemcpyDeviceToHost);
+			
+			
+			//printf("Q: ");
+			for(int z =0 ; z<V; z++){
+				
+				if( !isFixed[z] && Q[z]==1  )
+				{
+					//printf("%d(%d) ",z,d[z] );
+					H.insertOrDecrease(d[z],z);
+				}
+				Q[z]=0;
+			}
+			//printf("\n");
+			
+			block_dim = (V<512)?V:512 ;
+			grid_dim = (V+block_dim-1)/block_dim; 
+
+			get_next_R_gpu(graph,V,grid_dim,block_dim);
+			cudaMemcpy(R, R_gpu, V*sizeof(int), cudaMemcpyDeviceToHost);
+			Q_zeroes_out<<<grid_dim,block_dim>>>(graph_gpu, V, Q_gpu);
+			cudaDeviceSynchronize();
+		}
+
+	}
+	gettimeofday(&end,NULL);
+	unsigned long long startms=(unsigned long long)(start.tv_sec) * 1000 + (unsigned long long)(start.tv_usec) / 1000;
+	unsigned long long endms=(unsigned long long)(end.tv_sec) * 1000 + (unsigned long long)(end.tv_usec) / 1000;
+	
+	time=(int)(endms-startms);
+
+	int MST_total_weight=0;
+	//printf("prim_gpu T: ");
+	for(int i =0; i < V; i++)
+	{
+		//printf("%d ", T[i]);
+		MST_total_weight+=T[i];
+	}
+	//printf("\n");
+	var_free();
+	gpu_var_free();
+	return MST_total_weight;
+}
+int prim_mst_hybrid_bad_scan(Graph& g, int& time)
 {
 	struct timeval start;
 	struct timeval end;
